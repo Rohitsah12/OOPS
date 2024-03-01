47268798850271d94bba67a33bd21eeb9796676b
Rohit Kumar Sah <148933953+Rohitsah12@users.noreply.github.com>
2024-03-01 20:06:52 +0530

    Encapsulation

--- BasicProject/ATMprojects.py
+++ BasicProject/ATMprojects.py
@@ -1,14 +1,14 @@
 class Atm:
     def __init__(self):
-        self.pin=""
-        self.balance=0
-        self.menu()
-    def menu(self):
+        self.__pin=""
+        self.__balance=0
+        self.__menu()
+    def __menu(self):
         user_input=input("""
                 Welcome!! 
             1. Enter 1 to create pin
             2. Enter 2 to deposit
-            3. Enter 3 tp withdraw
+            3. Enter 3 to withdraw
             4. Enter 4 to check balance
             5. Enter 5 to exit
 """)
@@ -23,23 +23,23 @@ class Atm:
         else:
             print("Thankyou!! Have a Nice Day")
     def create_pin(self):
-        self.pin=int(input("Enter your pin: "))
+        self.__pin=int(input("Enter your pin: "))
         print("Pin Set Sucessfully")
     
     def deposit(self):
         temp=int(input("Enter your pin: "))
-        if temp==self.pin:
+        if temp==self.__pin:
             amount=int(input("Enter the amount: "))
-            self.balance=self.balance+amount
+            self.__balance=self.__balance+amount
             print("Deposit Sucessful")
         else:
             print("Invalid Pin")
     def withdraw(self):
         temp=int(input("Enter your pin: "))
-        if temp==self.pin:
+        if temp==self.__pin:
             amount=int(input("Enter the amount: "))
-            if amount<self.balance:
-                self.balance=self.balance-amount
+            if amount<self.__balance:
+                self.__balance=self.__balance-amount
                 print("Operation Sucessful")
             else:
                 print("Insufficient balance")
@@ -48,7 +48,7 @@ class Atm:
     
     def check_balance(self):
         temp=int(input("Enter your pin: "))
-        if temp==self.pin:
-            print(self.balance)
+        if temp==self.__pin:
+            print(self.__balance)
         else:
             print("Invalid pin")
